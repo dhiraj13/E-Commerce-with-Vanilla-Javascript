6ee9f3afe671a7a64577d16871a176933a0283e8
dhiraj khapangi <dkhapangi13@outlook.com>
2019-09-04 21:47:48 +0545

    transferring from local products.json data to contentful cms

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+const client = contentful.createClient({
+  // This is the space ID. A space is like a project folder in Contentful terms
+  space: "uxgzxm8jhc18",
+  // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
+  accessToken: 
+    "ZMizNscQ1wfEfCizI3jpv2IUvFvY1CRJGwZRKHMo1sQ"
+});
+
 //variables
 
 const cartBtn = document.querySelector(".cart-btn");
@@ -19,10 +27,14 @@ let buttonsDOM = [];
 class Products {
   async getProducts() {
     try {
-      let result = await fetch("products.json");
-      let data = await result.json();
+      let contentful = await client.getEntries({
+        content_type: "comfyHouseProduct"
+      });
 
-      let products = data.items;
+      // let result = await fetch("products.json");
+      // let data = await result.json();
+
+      let products = contentful.items;
       products = products.map(item => {
         const { title, price } = item.fields;
         const { id } = item.sys;
